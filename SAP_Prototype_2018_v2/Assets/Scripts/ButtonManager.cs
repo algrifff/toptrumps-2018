a9cc9013f3bf51208fe3c087eea782b9ea8f8dee
Alex Griffiths <ag@om.tv>
2018-07-16 19:08:01 +0200

    n

--- SAP_Prototype_2018_v2/Assets/Scripts/ButtonManager.cs
+++ SAP_Prototype_2018_v2/Assets/Scripts/ButtonManager.cs
@@ -5,13 +5,81 @@ using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	[SerializeField]
+	private GameObject mainMenu;
+	[SerializeField]
+	private GameObject topTrumpsMenu;
+	[SerializeField]
+	private GameObject t_hostJoinMenu;
+	[SerializeField]
+	private GameObject t_teamSelectMenu;
+	[SerializeField]
+	private GameObject drillsMenu;
+	[SerializeField]
+	private GameObject home;
+
+	public delegate void SceneChange(string sceneName);
+	public event SceneChange NewScene;
+	
+	private void Start()
+	{
+		mainMenu.SetActive(true);
+
+		topTrumpsMenu.SetActive(false);
+		t_teamSelectMenu.SetActive(false);
+		t_teamSelectMenu.SetActive(false);
+		drillsMenu.SetActive(false);
+		home.SetActive(false);
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void OnTopTrumpsClick()
+	{
+		mainMenu.SetActive(false);
+		topTrumpsMenu.SetActive(true);
+		t_hostJoinMenu.SetActive(true);
+		t_teamSelectMenu.SetActive(false);
+		home.SetActive(true);
+	}
+	public void OnDrillsClick()
+	{
+		mainMenu.SetActive(false);
+		drillsMenu.SetActive(true);
+		home.SetActive(true);
+	}
+
+	public void OnHostClick()
+	{
+		t_hostJoinMenu.SetActive(false);
+		t_teamSelectMenu.SetActive(true);
+		home.SetActive(true);
+	}
+
+	//top trumps team selection
+	public void OnTopTrumpsTeamSelectClick()
+	{
+		//load top trump scene (pass the scene to load to the scene manager)
+		Debug.Log("load top trumps scene");
+		NewScene("TopTrumps");
+	}
+
+	public void OnDrillsSelectClick()
+	{
+		//load drill scene (pass scene to load to scene manager)
+		Debug.Log("load drills scene");
+		NewScene("Drills");
+	}
+
+
+	public void OnHomeClick()
+	{
+		mainMenu.SetActive(true);
+
+		topTrumpsMenu.SetActive(false);
+		t_teamSelectMenu.SetActive(false);
+		t_teamSelectMenu.SetActive(false);
+		drillsMenu.SetActive(false);
+		home.SetActive(false);
+
 	}
 }
