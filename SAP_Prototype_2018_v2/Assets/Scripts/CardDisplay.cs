cf82ea6d86460e7d617a14c178ad73a6d1a60bd7
Alex Griffiths <ag@om.tv>
2018-07-17 16:33:19 +0200

    button updates

--- SAP_Prototype_2018_v2/Assets/Scripts/CardDisplay.cs
+++ SAP_Prototype_2018_v2/Assets/Scripts/CardDisplay.cs
@@ -12,8 +12,8 @@ public class CardDisplay : MonoBehaviour {
 	[SerializeField]
 	private Text positionText;
 
-	//text fields for stat name changes
-	[Header("Titles Text Fields")]
+	//text fields for 3d stat name changes
+	[Header("Titles Text Fields for 3D")]
 	[SerializeField]
 	private Text paceText;
 	[SerializeField]
@@ -27,8 +27,8 @@ public class CardDisplay : MonoBehaviour {
 	[SerializeField]
 	private Text strengthText;
 
-	// text fiels for number changes
-	[Header("Statistic Text Fields")]
+	// text fields for 3d number changes
+	[Header("Statistics for Text Fields for 3D")]
 	[SerializeField]
 	private Text paceIntText;
 	[SerializeField]
@@ -42,6 +42,34 @@ public class CardDisplay : MonoBehaviour {
 	[SerializeField]
 	private Text strengthIntText;
 
+	[Header("Titles for text fields 2D buttons")]
+	[SerializeField]
+	private Text paceButtonText;
+	[SerializeField]
+	private Text dribblingButtonText;
+	[SerializeField]
+	private Text shootingButtonText;
+	[SerializeField]
+	private Text defendingButtonText;
+	[SerializeField]
+	private Text passingButtonText;
+	[SerializeField]
+	private Text strengthButtonText;
+
+	[Header("Statistics for text fields 2D buttons")]
+	[SerializeField]
+	private Text paceButtonStatText;
+	[SerializeField]
+	private Text dribblingButtonStatText;
+	[SerializeField]
+	private Text shootingButtonStatText;
+	[SerializeField]
+	private Text defendingButtonStatText;
+	[SerializeField]
+	private Text passingButtonStatText;
+	[SerializeField]
+	private Text strengthButtonStatText;
+
 	private void OnEnable()
 	{
 		GameManager.SendCard += RecieveCard;
@@ -53,14 +81,15 @@ public class CardDisplay : MonoBehaviour {
 	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 
 	void RecieveCard(Card card)
 	{
 		currentCard = card;
-
+		//sets 3D object values
 		nameText.text = "" + card.name;
 		positionText.text = "" + card.position;
 		paceIntText.text = "" + card.pace;
@@ -70,23 +99,49 @@ public class CardDisplay : MonoBehaviour {
 		passingIntText.text = "" + card.passing;
 		strengthIntText.text = "" + card.strength;
 
-		if(card.position == "GK")
+		//sets 2D menu object values
+		paceButtonStatText.text = "" + card.pace;
+		dribblingButtonStatText.text = "" + card.dribbling;
+		shootingButtonStatText.text = "" + card.shooting;		
+		defendingButtonStatText.text = "" + card.defending;		
+		passingButtonStatText.text = "" + card.passing;
+		strengthButtonStatText.text = "" + card.strength;
+
+		if (card.position == "GK")
 		{
+			//3D text
 			paceText.text = "Diving";
 			dribblingText.text = "Handling";
 			shootingText.text = "Kicking";
 			defendingText.text = "Reflexes";
 			passingText.text = "Speed";
 			strengthText.text = "Positioning";
+
+			//2D text
+			paceButtonText.text = "Diving";
+			dribblingButtonText.text = "Handling";
+			shootingButtonText.text = "Kicking";
+			defendingButtonText.text = "Reflexes";
+			passingButtonText.text = "Speed";
+			strengthButtonText.text = "Positioning";
 		}
 		else
 		{
+			//3D text
 			paceText.text = "Pace";
 			dribblingText.text = "Dribbling";
 			shootingText.text = "Shooting";
 			defendingText.text = "Defending";
 			passingText.text = "Passing";
 			strengthText.text = "Strength";
+
+			//2D text
+			paceButtonText.text = "Pace";
+			dribblingButtonText.text = "Dribbling";
+			shootingButtonText.text = "Shooting";
+			defendingButtonText.text = "Defending";
+			passingButtonText.text = "Passing";
+			strengthButtonText.text = "Strength";
 		}
 
 	}
